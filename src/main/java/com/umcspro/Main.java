ae50d0ffabfd3ef7c2f4b7ad7c16ed6568f177dc
umcsuser <kwasniewiczl@office.umcs.pl>
2025-03-19 03:59:43 +0100

    add some changes :)

--- src/main/java/com/umcspro/Main.java
+++ src/main/java/com/umcspro/Main.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-
+//add some changes...
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
